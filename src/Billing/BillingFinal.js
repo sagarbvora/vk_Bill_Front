fd1bf642395a99a5cfab330c493c14e481330a0d
sagarbvora <sagarbvora10@gmail.com>
2021-10-02 12:34:03 +0530

    final print.

--- src/Billing/BillingFinal.js
+++ src/Billing/BillingFinal.js
@@ -1,17 +1,15 @@
-import React, {useState, useEffect, createRef} from 'react';
+import React, {useState, useEffect, useCallback, useRef } from 'react';
 import axios from "axios";
-// import ReactToPrint from 'react-to-print';
-// import LOGO from "../assert/images/logo.png";
-// import {useReactToPrint} from "react-to-print";
-import Pdf from "react-to-pdf";
 import { PrinterOutlined } from '@ant-design/icons';
-import "./Billing.scss";
-import BillingPage from "./BillingPage";
+import { jsPDF } from "jspdf";
+import { toPng, toJpeg, toCanvas } from 'html-to-image';
 import LOGO from "../assert/images/logo.png";
+import "./Billing.scss";
+
 const BillingFront = (props) =>{
     const [getDetails, setDetails] = useState({});
     const [total, setTotal] = useState(null);
-    const ref = createRef();
+    const ref = useRef(null);
     useEffect(() =>{
         const id = props && props.match.params.id;
         if (id) {
@@ -40,26 +38,45 @@ const BillingFront = (props) =>{
             console.log("Error", err);
         })
     };
-    // const handlePrint = useReactToPrint({
-    //     content: () => componentRef.current,
-    // });
+     const onButtonClick = useCallback(() => {
+        if (ref.current === null) {
+          return
+        }
+        var alias = Math.random().toString(35);
+        toCanvas(ref.current, { cacheBust: true, })
+          .then((canvas) => {
+                 var img = canvas.toDataURL("image/jpeg");
+                 const doc = new jsPDF({
+                     unit: 'mm',
+                     format: 'a4',
+                     compress: true
+                 });
+                var width = doc.internal.pageSize.getWidth();
+                var height = doc.internal.pageSize.getHeight();
+                doc.addImage(img, 'JPEG', 10, 10, width-20, height-20);
+                doc.save('invoice.pdf');
+          })
+          .catch((err) => {
+            console.log(err)
+          })
+      }, [ref])
+
     return(
         <div>
-            <div className="print mb-2" align="end">
-                <Pdf targetRef={ref} filename="bill.pdf">
-                    {({toPdf}) =><button className="btn btn-primary" onClick={toPdf}><PrinterOutlined style={{fontSize: '18px', paddingRight: "10px"}} />Print</button>}
-                </Pdf>
+            <div className="mt-3">
+                <button className="btn btn-primary" onClick={onButtonClick}>Download</button>
             </div>
-            {/*<BillingPage ref={ref} getDetails={getDetails} total={total}/>*/}
-            <div className="final-print row d-flex justify-content-center mt-3" ref={ref}>
-                <div className="col-sm-8">
-                    <div className="card">
+            <div className="final-print row mb-3 mt-3" id="print">
+                <div className="col-sm-3"/>
+                <div className="col-sm-6">
+                    <div ref={ref}>
+                    <div className="card ml-2 mr-2">
                         <div className="card-body">
                             <div className="row bill-header">
-                                <div className="col-sm-2 col-md-2 logo">
-                                    <img src={LOGO} alt="logo" height="71"/>
+                                <div className="col-sm-2 col-md-3 logo">
+                                    <img src={LOGO} alt="logo" height="71" className="mt-3"/>
                                 </div>
-                                <div className="content col-sm-10 col-md-10 justify-content-center">
+                                <div className="content col-sm-10 col-md-9 justify-content-center">
                                     <h4 className="heading-vk">VK-ENTERPRISE</h4>
                                     <p>Shop No. 1, Dan Ashish Soc., Gate No.3, Chikuwadi Road, Katargam, Surat-395004</p>
                                     <p><b>Contact Us : 87806 30940</b></p>
@@ -126,31 +143,35 @@ const BillingFront = (props) =>{
                                                 if(otherNumbers !== '')
                                                     lastThree = ',' + lastThree;
                                                 return (<tr key={i} style={{"borderBottom": "none"}}>
-                                                    <td>{i + 1}</td>
-                                                    <td>{item.description || "----"}</td>
-                                                    <td>{item.quantity || "0"}</td>
-                                                    <td>{item.rate || "0"}</td>
+                                                    <td><div>{i + 1}</div></td>
+                                                    <td><div>{item.description || "----"}</div></td>
+                                                    <td><div>{item.quantity || "0"}</div></td>
+                                                    <td><div>{item.rate || "0"}</div></td>
                                                     <td>{otherNumbers.replace(/\B(?=(\d{2})+(?!\d))/g, ",") + lastThree}</td>
                                                 </tr>)})
                                         }
                                         </tbody>
                                         <img src={LOGO} alt="logo" className="back-logo" height="100" width="150"/>
                                     </table>
-                                    <div className="total"><span><b>TOTAL</b></span><span>{total || "0"}</span></div>
+                                    <div className="total"><span><b>TOTAL</b></span><span>{total + '/-'}</span></div>
                                     <div className="terms-condition">
                                         <span><b>TERMS & CONDITIONS:</b></span>
                                         <ul>
                                             <li>Warranty as per respective company's Terms & Condition.</li>
                                             <li>No Warranty on Burn or Physically Damaged Goods.</li>
-                                            <li>Your payment </li>
                                             <li>GOODs will be dispatched at buyers risk.</li>
                                         </ul>
+                                        <div className="d-flex justify-content-end">
+                                            <p><b>Authorised Signatory</b></p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+                    </div>
                 </div>
+                <div className="col-sm-3"/>
             </div>
         </div>
     )
